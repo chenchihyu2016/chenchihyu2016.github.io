609e865016cc531bac60110f982002c58adfe991
Chih-Yu, Chen <chenchihyu2014@gmail.com>
2022-05-17 13:46:21 +0800

    First Release

--- Error.1651592576816.js
+++
@@ -1,5 +0,0 @@
-<<<<<<< HEAD:dist/Error.1651592576816.js
-import{_ as r,d as t,u as o,o as s,c as _,t as a}from"./index.1651592576816.js";const n=t({setup(){const{t:e}=o();return{t:e}}}),c={class:"error_page_container"};function p(e,d,i,u,l,f){return s(),_("div",c,a(e.t("PAGE_NOT_FOUND")),1)}var m=r(n,[["render",p],["__scopeId","data-v-ed90746a"]]);export{m as default};
-=======
-import{_ as r,d as t,u as o,o as s,c as _,t as a}from"./index.1651548264420.js";const n=t({setup(){const{t:e}=o();return{t:e}}}),c={class:"error_page_container"};function p(e,d,i,u,l,f){return s(),_("div",c,a(e.t("PAGE_NOT_FOUND")),1)}var m=r(n,[["render",p],["__scopeId","data-v-ed90746a"]]);export{m as default};
->>>>>>> ffdfb7070311da10ccd5cf18405126e7f3926d15:dist/Error.1651548264420.js
---
+++ Reading.1652766286611.js
@@ -0,0 +1,1 @@
+import{a as s}from"./image.1652766286611.js";import{_ as t,d as a,o as r,c as o}from"./index.1652766286611.js";const _=a({setup(){return{useGetImageUrl:s}}}),n=["src"];function c(e,p,i,d,m,u){return r(),o("img",{class:"image_perspective",src:e.useGetImageUrl("AtomicHabits")},null,8,n)}var g=t(_,[["render",c],["__scopeId","data-v-47612602"]]);export{g as default};

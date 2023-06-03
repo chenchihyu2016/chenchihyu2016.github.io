61a46ceda95ce68322208ff73f389034ea88f62c
Chih-Yu, Chen <chenchihyu2014@gmail.com>
2023-06-04 00:19:15 +0800

    Modify issue of parallax init error.

---
+++ Error.1685809095270.js
@@ -0,0 +1,1 @@
+import{_ as t,d as o,o as r,c as _,t as s}from"./index.1685809095270.js";const n=o({setup(){return{}}}),a={class:"error_page_container"};function c(e,p,d,i,u,l){return r(),_("main",a,s(e.$t("PAGE_NOT_FOUND")),1)}const m=t(n,[["render",c],["__scopeId","data-v-7bb340d4"]]);export{m as default};
